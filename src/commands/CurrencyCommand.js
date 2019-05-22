109ca13eb0365707e6009a5bc1f1406368df4c6f
MrGaabriel <hello@mrgaabriel.space>
2019-05-21 21:43:49 -0300

    Mudar a forma de como as cotações são pegas

--- src/commands/CurrencyCommand.js
+++ src/commands/CurrencyCommand.js
@@ -1,6 +1,7 @@
 const Command = require("../structures/Command")
 
 const axios = require("axios")
+const cheerio = require("cheerio")
 
 class CurrencyCommand extends Command {
 
@@ -19,12 +20,25 @@ class CurrencyCommand extends Command {
 		const from = args[0]
 		const to = args[1]
 
-		const quantity = parseInt(args[2]) || 1
+		const quantity = parseFloat(args[2]) || 1
 
-		const request = await axios.get("https://api.exchangeratesapi.io/latest?base=" + from, { "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.27 Safari/537.36" })
-		const converted = request.data.rates[to] * quantity
+		const converted = await this.convert(quantity, from, to)
 
-		message.reply(`**\`${from}\`** -> **\`${to}\` »** \`${converted} ${to}\` 💸`)
+		message.reply(`**\`${quantity} ${from}\`** -> **\`${to}\` »** \`${converted} ${to}\` 💸`)
+	}
+	
+	async convert(quantity, from, to) {
+		const request = await axios.get(`https://www.investing.com/currencies/${from.toLowerCase()}-${to.toLowerCase()}`)
+		const body = request.data
+
+		const $ = cheerio.load(body)
+
+		const text = $('#last_last').text()
+		const rate = parseFloat(text)
+
+		const converted = rate * quantity
+
+		return converted
 	}
 }
 
